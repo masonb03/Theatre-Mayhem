9bfdcbee1de87b11f09501f73beb919de0f27eb7
Mason <bevinsm355@gmail.com>
2026-01-17 21:12:53 -0500

    Updated single movie page

--- movies.js
+++ movies.js
@@ -15,11 +15,11 @@ function movieHTML(movie) {
     <div class="single__movie--info">
         <h3 class="single__movie--title">${movie.Title}</h3>
         <ul class="single__movie__misc--info">
-            <li class="single__movie--fact">Duration: ${movie.Runtime}</li>
-            <li class="single__movie--fact">Rating: ${movie.Rated}</li>
-            <li class="single__movie--fact">Released: ${movie.Released}</li>
+            <li class="single__movie--fact"><b>Duration</b>: ${movie.Runtime}</li>
+            <li class="single__movie--fact"><b>Rating</b>: ${movie.Rated}</li>
+            <li class="single__movie--fact"><b>Released</b>: ${movie.Released}</li>
         </ul>
-        <p class="single__movie--fact">${movie.Genre}</p>
+        <p class="single__movie--fact"><b>Genre:</b> ${movie.Genre}</p>
         <p class = "single__movie--fact"><b>Writer:</b> ${movie.Writer}</p>
         <p class = "single__movie--fact"><b>Actors: </b>${movie.Actors}</p>
         <p class = "single__movie--fact"><b>Plot:</b> ${movie.Plot}</p>
